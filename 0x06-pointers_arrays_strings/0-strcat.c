b6fe9ad6512d25a3e94f3bb4da075a0212ebc985
M0hdii <mehdiomar71@gmail.com>
2023-08-24 03:45:37 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strcat - Concatenates two strings.
+ * _strcat - Concatenates two strings.
  * @dest: A pointer to the string to be concatenated.
  * @src: The source string.
  * Return: pointer to the destination string @dest.
